958030f04e66f6b684b05508e9e5aeba9315d710
RupeshAryal <aryalrupesh2074@gmail.com>
2022-09-05 15:12:10 +0545

    added few more functions

--- 3. Linked List/CircularLinkedlist.c
+++ 3. Linked List/CircularLinkedlist.c
@@ -56,7 +56,7 @@ int Length(struct Node *p)
     {
         len++;
         p = p->next;
-    }
+    }while(p!=Head);
     return len;
 
 }
@@ -92,10 +92,46 @@ void Insert(struct Node *p, int index, int x)
     }
 }
 
+int Delete(struct Node *p, int index)
+{
+    struct Node *q;
+    int x;
+
+    if(index<0 || index>Length(p))
+        return -1;
+    if(index == 1)
+    {
+        while(p->next != Head) p= p->next;
+        x = Head->data;
+        if(Head == p)
+        {
+            free(Head);
+            Head = NULL;
+        }
+        else
+        {
+            p->next = Head->next;
+            free(Head);
+            Head=p->next;
+        }
+    }
+    else
+    {
+        for(int i = 0; i<index - 2;i++)
+            p=p->next;
+        q = p->next;
+        p->next = q->next;
+        x = q->data;
+        free(q);
+    }
+    return x;
+}
 int main()
 {
     int A[] = {2,3,4,5,6};
     create(A,5);
+
+    Delete(Head,1);
     RDisplay(Head);
     return 0;
 }
