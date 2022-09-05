16f908001e9483a5311ae2c663d1448168803c7d
RupeshAryal <aryalrupesh2074@gmail.com>
2022-09-05 14:40:42 +0545

    included linked list implementation

---
+++ 4.Stack/StackLinkedList.c
@@ -0,0 +1,67 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*top = NULL;
+
+
+void Push(int x)
+{
+    struct Node *t;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    if (t == NULL)
+        printf("stack overflow");
+    else
+    {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+int pop()
+{
+    int x = -1;
+    struct Node *t;
+    if(top == NULL)
+        printf("stack underflow");
+    else
+    {
+        t = top;
+        top = top->next;
+        x = t->data;
+        free(t);
+    }
+    return x;
+}
+
+void Display()
+{
+    struct Node *t;
+    t = top;
+    while(t!=NULL)
+    {
+        printf("%d ", t->data);
+        t = t->next;
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    Push(19);
+    Push(11);
+    Push(12);
+    Push(13);
+    pop();
+    pop();
+
+
+    Display();
+    return 0;
+}
